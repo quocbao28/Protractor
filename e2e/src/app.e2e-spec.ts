84558ce361f8aa73400009a890af1307c44beabb
Bao Dao Quoc <dqbao@aavn.local>
2019-05-06 17:32:07 +0700

    1st tcs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to protractor-demo!');
+    expect(page.getTitleText()).toEqual('Contacts');
   });
 
   afterEach(async () => {
